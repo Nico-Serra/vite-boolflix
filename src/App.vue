5590afa8567797f4ca7d2b60c68a0f6f1d7e5465
Nico-Serra <nico.serra000@gmail.com>
2024-04-03 15:49:23 +0200

    add rating star at movie and serie

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ export default {
       movieApi: 'https://api.themoviedb.org/3/search/movie?api_key=3d400e0ed0f9cd54b51de374af445077&query=',
       serieApi: 'https://api.themoviedb.org/3/search/tv?api_key=3d400e0ed0f9cd54b51de374af445077&query=',
       userSearchValue: '',
+      rating:[],
     }
   },
   methods: {
@@ -29,7 +30,7 @@ export default {
     <button type="button" @click="search()">Search</button>
   </div>
 
-  <div>
+  <div v-if="state.movies.length > 0">
     <h2>FILM</h2>
     <ul>
       <li v-for="movie in state.movies">
@@ -38,14 +39,16 @@ export default {
         <i :class="'fi fi-' + movie.original_language"
           v-if="movie.original_language != 'en' && movie.original_language != 'ja' && movie.original_language != 'zh'"></i>
 
-        <img src="/public/img/en.png" alt="" width="30px" v-if="movie.original_language === 'en'"> <img
-          src="/public/img/ja.png" alt="" width="30px" v-if="movie.original_language === 'ja'"> <img
-          src="/public/img/zh.png" alt="" width="25px" v-if="movie.original_language === 'zh'">, {{ movie.vote_average }}
+        <img src="/img/en.png" alt="" width="30px" v-if="movie.original_language === 'en'"> <img
+          src="/img/ja.png" alt="" width="30px" v-if="movie.original_language === 'ja'"> <img
+          src="/img/zh.png" alt="" width="25px" v-if="movie.original_language === 'zh'">,
+          <i class="fa fa-star" v-for="star in Math.round(movie.vote_average.toFixed(0)/2)"></i>
+          <i class="fa-regular fa-star"  v-for="starEmpty in 5-Math.round(movie.vote_average.toFixed(0)/2)"></i>
       </li>
     </ul>
   </div>
 
-  <div>
+  <div v-if="state.series.length > 0">
     <h2>SERIE TV</h2>
     <ul>
       <li v-for="serie in state.series">
@@ -54,9 +57,11 @@ export default {
         <i :class="'fi fi-' + serie.original_language"
           v-if="serie.original_language != 'en' && serie.original_language != 'ja' && serie.original_language != 'zh'"></i>
 
-        <img src="/public/img/en.png" alt="" width="30px" v-if="serie.original_language === 'en'"> <img
-          src="/public/img/ja.png" alt="" width="30px" v-if="serie.original_language === 'ja'"> <img
-          src="/public/img/zh.png" alt="" width="25px" v-if="serie.original_language === 'zh'">, {{ serie.vote_average }}
+        <img src="/img/en.png" alt="" width="30px" v-if="serie.original_language === 'en'"> <img
+          src="/img/ja.png" alt="" width="30px" v-if="serie.original_language === 'ja'"> <img
+          src="/img/zh.png" alt="" width="25px" v-if="serie.original_language === 'zh'">, 
+          <i class="fa fa-star" v-for="star in Math.round(serie.vote_average.toFixed(0)/2)"></i>
+          <i class="fa-regular fa-star"  v-for="starEmpty in 5-Math.round(serie.vote_average.toFixed(0)/2)"></i>
       </li>
     </ul>
   </div>
