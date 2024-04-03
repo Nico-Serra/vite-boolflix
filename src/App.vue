4af8fe08b85afca215d0ae946c3ceb20582d927d
Nico-Serra <nico.serra000@gmail.com>
2024-04-03 12:00:05 +0200

    add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,30 @@ export default {
   data(){
     return{
       state,
-      movieApi: 'https://api.themoviedb.org/3/search/movie?api_key=3d400e0ed0f9cd54b51de374af445077&query=avengers',
+      movieApi: 'https://api.themoviedb.org/3/search/movie?api_key=3d400e0ed0f9cd54b51de374af445077&query=',
+      userSearchValue: '',
+    }
+  },
+  methods: {
+    searchMovie(){
+      state.addElement(`${this.movieApi}+${this.userSearchValue}`)
     }
   },
   mounted(){
-    state.addElement(this.movieApi),
-    console.log(this.state.results);
+
   }
 }
 </script>
 
 <template>
-  <!-- <ul>
-    <li v-for="movie in state.movies">{{ movie.title }}</li>
-  </ul> -->
+  <div>
+    <input type="text" v-model="userSearchValue">
+    <button type="button" @click="searchMovie()">Search</button>
+  </div>
+
+  <ul>
+    <li v-for="movie in state.movies">{{ movie.title }} , {{ movie.original_title }} , {{ movie.original_language }} , {{ movie.vote_average }}</li>
+  </ul>
 </template>
 
 <style>
